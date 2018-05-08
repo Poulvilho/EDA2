136c33f1c2573dcc3edf2dd261d52072f76559bb
Bruno Dantas <oliveiradantas96@gmail.com>
2018-05-08 17:00:28 -0300

    implementando a parte decimal.

--- Trabalho_5/Trabalho_5.c
+++ Trabalho_5/Trabalho_5.c
@@ -1,25 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <string.h>
 
 int main(){
 
     int notas[7]= {100, 50, 20, 10, 5, 2, 1};
-    int i = 0, troco = 0, aux[7];
-    int tamanho = sizeof notas / sizeof *notas;
+    int moedas[5] = {50, 25, 10, 5, 1};
+    int i = 0, inteiro = 0, decimal = 0, aux[7], aux2[5];
 
-    printf("Insira o valor do troco:\n");
-    scanf("%d",&troco);
+    int tamanho_moedas = sizeof moedas / sizeof *moedas;
+    int tamanho_notas = sizeof notas / sizeof *notas;
 
-    for (i; i < tamanho; i++) {
-    /* usa seqvalor[i] para cada moeda */
-        aux[i] = troco/notas[i];
-        troco = troco % notas[i];
-}
-    printf("Foram utilizadas as seguintes notas para o troco:\n");
-    for (i = 0; i < tamanho; i++){
-        printf("%d notas de %d \n", aux[i], notas[i]);
+    printf("Insira o valor do troco: Ex:(00,00)\n");
+    scanf("%d,%d", &inteiro, &decimal);
+
+    for (i; i < tamanho_notas; i++) {
+        aux[i] = inteiro/notas[i];
+        inteiro = inteiro % notas[i];
+    }
+
+    for (i = 0; i < tamanho_moedas; i++) {
+        aux2[i] = decimal/moedas[i];
+        decimal = decimal % moedas[i];
+    }
+    printf("Foram utilizadas as seguintes notas e moedas para o troco:\n");
+    for (i = 0; i < tamanho_notas; i++){
+        printf("%d notas de $%d \n", aux[i], notas[i]);
+    }
+    printf("\n");
+    for (i = 0; i < tamanho_moedas; i++){
+        printf("%d moedas de ¢%d \n", aux2[i], moedas[i]);
     }
 
 return 0;
