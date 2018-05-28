a7a3fdd8e8cde4ddd356c1181c115ca174f85ce2
Paulo Victor de Menezes Lopes <pv.paulovictor.vp@gmail.com>
2018-05-28 13:02:28 -0300

    Colocando loop no menu e print de cada interação

--- Trabalho_6/README.md
+++ Trabalho_6/README.md
@@ -1,13 +1,16 @@
 # Trabalho 6
 
 Este é o trabalho 6, referente ao código de algoritmo dividir para conquistar realizado por:
+
 ##	150120371 - Bruno Oliveira Dantas
 ##	160016428 - Paulo Victor de Menezes Lopes.
 
-Para o trabalho foi implementado o Merge Sort, que é um modelo de Dividir para conquista, que mostra de forma básica o funcionamento de tal algoritmo.
+Para o trabalho foi implementado o Merge Sort Interativo, que é um modelo de Dividir para Conquistar, que mostra de forma básica o funcionamento de tal algoritmo.
 
 Tal trabalho foi desenvolvimento em C. Para rodá-lo é bem fácil:
 
-1)Compile o programa: gcc trabalho6.c -o nome_executavel
+1) Compile o programa: gcc trabalho6.c -o < nome_executavel >;
 
-2) Execute: ./nome_executavel
+2) Execute: ./< nome_executavel >;
+
+3) Siga as instrunções passada pelo programa;
--- Trabalho_6/trabalho6.c
+++ Trabalho_6/trabalho6.c
@@ -3,135 +3,134 @@
 #include <time.h>
 #include <math.h>
 
-void swap(int vector[], int i, int j){
+void Merge(int vector[], int start, int middle, int end, int vector_size) {
+    int i = start, j = middle + 1, k = start;
+    int *vector_aux = (int*)malloc(vector_size * sizeof(int));
+    int y = 0;
 
-    int aux = vector[i];
-    vector[i] = vector[j];
-    vector[j] = aux;
-
-}
-
-void Merge(int vector[], int start, int middle, int end, int vector_size){
-
-  int i = start, j = middle + 1, k = start;
-
-  int *vector_aux = (int*)malloc(vector_size * sizeof(int));
-  int y = 0;
-
-  while(y < vector_size){
-    vector_aux[y] = vector[y];
-    y++;
-  }
-
-  while(i <= middle && j <= end){
-    if(vector_aux[i] < vector_aux[j]){
-      vector[k] = vector_aux[i];
-      i++;
+    while(y < vector_size) {
+        vector_aux[y] = vector[y];
+        y++;
     }
-    else{
-      vector[k] = vector_aux[j];
-      j++;
+
+    while(i <= middle && j <= end) {
+        if(vector_aux[i] < vector_aux[j]) {
+            vector[k] = vector_aux[i];
+            i++;
+        } else {
+            vector[k] = vector_aux[j];
+            j++;
+        }
+        k++;
     }
-    k++;
-  }
 
-  while(i <= middle){
-    vector[k] = vector_aux[i];
-    i++;
-    k++;
-  }
-
-  while(j <= end){
-    vector[k] = vector_aux[j];
-    j++;
-    k++;
-  }
-
-  free(vector_aux);
-}
-
-void MergeSortRecursivo(int vector[], int start, int end, int vector_size){
-  if(start < end){
-    int middle = (start + end)/2;
-
-    MergeSortRecursivo(vector,start,middle,vector_size);
-    MergeSortRecursivo(vector,middle+1,end,vector_size);
-    Merge(vector,start,middle,end,vector_size);
-  }
-}
-
-void MergeSortIterativo(int vector[], int vector_size){
-  int i = 0, subEnd = 0, slot = 2,middle, end = vector_size - 1;
-
-  while(slot/2 <= vector_size){
-    for(i = 0; i < vector_size; i+= slot){
-      subEnd = i + (slot - 1);
-      middle = (i + subEnd)/2;
-      if(subEnd > end){
-        subEnd = end;
-      }
-
-      Merge(vector,i,middle,subEnd,vector_size);
+    while(i <= middle) {
+        vector[k] = vector_aux[i];
+        i++;
+        k++;
     }
-    slot = slot*2;
-  }
+
+    while(j <= end) {
+        vector[k] = vector_aux[j];
+        j++;
+        k++;
+    }
+
+    free(vector_aux);
 }
 
-int main(){
+void MergeSortIterativo(int vector[], int vector_size) {
+    int subEnd = 0, slot = 2, middle, end = vector_size - 1;
 
-  int qtd_numero = 0, i = 0, numero_vetor = 0, option = 0;  
+    printf("Vetor Ordenando\n");
 
-  printf("*** MENU ***\n");
-  printf("0 - Sair.\n");
-  printf("1 - Inserir os Números manualmente.\n");
-  printf("2 - Inserir os Números aleatoriamente.\n");
-  printf("Escolha uma opção: ");
-  scanf("%d", &option);
+    while(slot / 2 <= vector_size) {
+        for(int i = 0; i < vector_size; i += slot) {
+            subEnd = i + (slot - 1);
+            middle = (i + subEnd) / 2;
+            if(subEnd > end) {
+                subEnd = end;
+            }
+            Merge(vector, i, middle, subEnd, vector_size);
+        }
+        printf("\n[ ");
+        for(int i = 0; i <= vector_size -1; i++){
+            printf("%d ",vector[i]);
+        }
+        printf("]\n\n");
 
-  if (option == 0) {
-    printf("Saindo...\n");
-    return 1;
-  }
-    printf("Insira a quantidade de numeros que deseja ordenar: \n");
+        slot *= 2;
+    }
+}
+
+void lerNumeros(int option) {
+    printf("\nInsira a quantidade de numeros que deseja ordenar: ");
+    int qtd_numero = 0, numero_vetor = 0;
     scanf("%d", &qtd_numero);
-      
-    int vector[qtd_numero];
-
-    int vector_size = qtd_numero;
   
+    int vector[qtd_numero];
+    int vector_size = qtd_numero;
+
     switch(option) {
         case 1:
             printf("Insira os numeros que deseja ordenar: \n");
-            for(i=0; i <= vector_size -1; i++) {
+            for(int i = 0; i <= vector_size - 1; i++) {
                 scanf("%d", &numero_vetor);
                 vector[i] = numero_vetor;
             }
         break;
+
         case 2:
-            for(i=0; i <= vector_size -1; i++) {
+            for(int i = 0; i <= vector_size - 1; i++) {
                 vector[i] = rand() % 100;
             }
         break;
-        default:
-            printf("Entrada Invalida!\n");    
     }
 
-  printf("\nVetor:\n\n[ ");
+    printf("\nVetor:\n\n[ ");
 
-  for(i = 0;i <= vector_size -1;i++){
-    printf("%d ",vector[i]);
-  }
-  printf("]\n\n");
+    for(int i = 0; i <= vector_size - 1; i++){
+        printf("%d ",vector[i]);
+    }
+    printf("]\n\n");
 
-  MergeSortIterativo(vector,vector_size);
+    MergeSortIterativo(vector,vector_size);
 
-  printf("\nVetor Ordenado:\n\n[ ");
+    printf("Vetor Ordenado:\n\n[ ");
 
-  for(i = 0;i <= vector_size -1;i++){
-    printf("%d ",vector[i]);
-  }
-  printf("]\n\n");
+    for(int i = 0; i <= vector_size - 1; i++){
+        printf("%d ",vector[i]);
+    }
+    printf("]\n\n");
+}
 
-  return 0;
+void menu() {
+    printf("*** MENU ***\n");
+    printf("0 - Sair.\n");
+    printf("1 - Inserir os Números manualmente.\n");
+    printf("2 - Inserir os Números aleatoriamente.\n");
+    printf("Escolha uma opcao: ");
+}
 
+int main() {
+    int option = 0;  
+    do {
+        menu();
+        scanf("%d", &option);
+
+        switch(option) {
+            case 0:
+                printf("\nSaindo...\n");
+            break;
+
+            case 1 ... 2:
+                lerNumeros(option);
+            break;
+            default:
+                printf("ENTRADA INVALIDA!\n\n");
+            break;
+        }
+    } while(option != 0 && option != 1 && option != 2);
+
+    return 0;
 }
