b1976b75454d623d3d8c472470d484ef433675c0
Paulo Victor de Menezes Lopes <pv.paulovictor.vp@gmail.com>
2018-05-30 07:05:19 -0300

    Adicionando Trabalho 7

---
+++ Trabalho_7/README.md
@@ -0,0 +1,16 @@
+# Trabalho 7
+
+Este é o trabalho 7 com o código referente à Programação Dinêmica realizado por:
+
+##	150120371 - Bruno Oliveira Dantas
+##	160016428 - Paulo Victor de Menezes Lopes.
+
+Para o trabalho foi implementado o código de achar o maior subsequência crescente dentro de uma vetor, que é resolvido por programação dinâmica, e tem complexidade O(n²).
+
+Tal trabalho foi desenvolvimento em C. Para rodá-lo é bem fácil:
+
+1) Compile o programa: gcc trabalho7.c -o < nome_executavel >;
+
+2) Execute: ./< nome_executavel >;
+
+3) Siga as instrunções passada pelo programa;
---
+++ Trabalho_7/trabalho7.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void maiorSubsequencia(int vector[], int qtd_numero) {
+	int tamanho[qtd_numero];
+	int anterior[qtd_numero];
+	for (int j = 0; j < qtd_numero; j++) {
+		tamanho[j] = 1;
+		anterior[j] = -1;
+		for (int i = 0; i < j; i++) {
+			if (vector[i] <= vector[j] && 1 + tamanho[i] > tamanho[j]) {
+				tamanho[j] = 1 + tamanho[i];
+				anterior[j] = i;
+            }
+		}
+	}
+
+    printf("Vetor do Tamanho das Sequencias: \n\n[ ");
+    for (int i = 0; i < qtd_numero; i++) {
+        printf("%d ", tamanho[i]);
+    }
+    printf("]\n\n");
+
+    printf("Vetor dos Predecessores: \n\n[ ");
+    for (int i = 0; i < qtd_numero; i++) {
+        printf("%d ", anterior[i]);
+    }
+    printf("]\n\n");
+
+    int MSC = 0;
+    for (int i = 0; i < qtd_numero; i++) {
+        if (MSC < tamanho[i]) {
+            MSC = tamanho[i];
+        }
+    }
+
+    printf("Tamanho da Maior Subsequencia: %d\n\n", MSC);
+
+    int posicaoMaior;
+    for (posicaoMaior = 0; tamanho[posicaoMaior] != MSC; posicaoMaior++) {
+    }
+
+    int maiorSubsequencia[MSC];
+    for (int i = MSC; i >= 0; i--) {
+        maiorSubsequencia[i - 1] = vector[posicaoMaior];
+        posicaoMaior = anterior[posicaoMaior];
+    }
+
+    printf("Maior Subsequencia: \n\n[ ");
+    for (int i = 0; i < MSC; i++) {
+        printf("%d ", maiorSubsequencia[i]);
+    }
+    printf("]\n\n");
+}
+
+void lerNumeros(int option) {
+    printf("\nInsira a quantidade de numeros que deseja ordenar: ");
+    int qtd_numero = 0;
+    scanf("%d", &qtd_numero);
+  
+    int vector[qtd_numero];
+
+    switch(option) {
+        case 1:
+            printf("Insira os numeros que deseja ordenar: \n");
+            for(int i = 0; i < qtd_numero; i++) {
+                scanf("%d", &vector[i]);
+            }
+        break;
+
+        case 2:
+            for(int i = 0; i < qtd_numero; i++) {
+                vector[i] = rand() % 100;
+            }
+        break;
+    }
+
+    printf("\nVetor:\n\n[ ");
+
+    for(int i = 0; i < qtd_numero; i++){
+        printf("%d ",vector[i]);
+    }
+    printf("]\n\n");
+
+    maiorSubsequencia(vector, qtd_numero);
+}
+
+void menu() {
+    printf("*** MENU ***\n");
+    printf("0 - Sair.\n");
+    printf("1 - Inserir os Números manualmente.\n");
+    printf("2 - Inserir os Números aleatoriamente.\n");
+    printf("Escolha uma opcao: ");
+}
+
+int main() {
+    int option = 0;  
+    do {
+        menu();
+        scanf("%d", &option);
+
+        switch(option) {
+            case 0:
+                printf("\nSaindo...\n");
+            break;
+
+            case 1 ... 2:
+                lerNumeros(option);
+            break;
+            default:
+                printf("ENTRADA INVALIDA!\n\n");
+            break;
+        }
+    } while(option != 0 && option != 1 && option != 2);
+
+    return 0;
+}
